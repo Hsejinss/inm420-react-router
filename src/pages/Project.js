500ac4900842b3847327ba48fed5181423622827
Hsejinss <84485920+Hsejinss@users.noreply.github.com>
2021-07-12 22:45:42 -0400

    project

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -2,13 +2,23 @@ import React from 'react';
 // import './Project.css';
 import './Style.css';
 
+
 const Project = (props) => {
   return(
     <div className="project-component">
       This is Project Component.
-      {/* content */}
+      {/* project content */}
       <div className="project-content">
-        
+        <div className="content1">
+          <h1 className="project-content-text">My Project</h1>
+        </div>
+      </div>
+      {/* project content grid */}
+      <div className="row grid-container-1">
+        <div className="col col-6 col-md-6 project-design-container">
+          <section ClassName="project-content-1">
+          </section>
+        </div>
       </div>
     </div>
   )
